54caadab1126c047dbcb5d99f644191509c7cda1
Paul Hrdina <qhrdina@fi.muni.cz>
2022-02-28 11:15:31 +0100

    Inlining all expectedStatement local variables in CustomerStatementTest

--- src/test/java/cz/muni/fi/pv260/videostore/CustomerStatementTest.java
+++ src/test/java/cz/muni/fi/pv260/videostore/CustomerStatementTest.java
@@ -10,36 +10,33 @@ class CustomerStatementTest {
 
     @Test
     void noRentals() {
-        String expectedStatement = """
+        assertStatement("""
                 Rental Record for John Doe
                 You owed 0.0
                 You earned 0 frequent renter points
-                """;
-        assertStatement(expectedStatement);
+                """);
     }
 
     @Test
     void regularMovieFlatRate() {
         customer.addRental(new Rental(new Movie("Regular Movie", Movie.REGULAR), 1));
-        String expectedStatement = """
+        assertStatement("""
                 Rental Record for John Doe
                 	Regular Movie	2.0
                 You owed 2.0
                 You earned 1 frequent renter points
-                """;
-        assertStatement(expectedStatement);
+                """);
     }
 
     @Test
     void regularMovieProgressiveRate() {
         customer.addRental(new Rental(new Movie("Regular Movie", Movie.REGULAR), 3));
-        String expectedStatement = """
+        assertStatement("""
                 Rental Record for John Doe
                 	Regular Movie	3.5
                 You owed 3.5
                 You earned 1 frequent renter points
-                """;
-        assertStatement(expectedStatement);
+                """);
     }
 
     private void assertStatement(String expectedStatement) {
