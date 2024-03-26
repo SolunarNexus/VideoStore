dfe15162953073a5e68eb40aab52c74a5415715e
Petr Kabourek <242739@mail.muni.cz>
2024-03-26 15:47:28 +0100

    add RegularMovie type

---
+++ src/main/java/cz/muni/fi/pv260/videostore/RegularMovie.java
@@ -0,0 +1,16 @@
+package cz.muni.fi.pv260.videostore;
+
+public class RegularMovie extends Movie {
+
+    public RegularMovie(String title) {
+        super(title, Movie.REGULAR);
+    }
+
+    @Override
+    public double getPriceOf(int daysRented) {
+        double price = 2;
+        if (daysRented > 2)
+            price += (daysRented - 2) * 1.5;
+        return price;
+    }
+}
