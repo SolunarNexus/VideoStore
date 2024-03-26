fe33ce8419b2dcbcacd83ea3036c31255b4cc79f
Petr Kabourek <242739@mail.muni.cz>
2024-03-26 15:54:35 +0100

    frequenterPoints special method in NewReleaseMovie

--- src/main/java/cz/muni/fi/pv260/videostore/NewReleaseMovie.java
+++ src/main/java/cz/muni/fi/pv260/videostore/NewReleaseMovie.java
@@ -10,4 +10,9 @@ public class NewReleaseMovie extends Movie {
     public double getPriceOf(int daysRented) {
         return daysRented * 3;
     }
+
+    @Override
+    int getFrequenterPoints(int daysRented) {
+        return daysRented > 1 ? 2 : 1;
+    }
 }
