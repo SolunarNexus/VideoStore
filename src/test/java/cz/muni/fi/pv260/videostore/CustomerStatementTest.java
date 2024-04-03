db1ecefb99bf27a5ba705127717326ff051e50d4
vaclav.bartak <vaclav.bartak@bringauto.com>
2024-04-03 14:38:43 +0200

    htmlStatement tests - equivalent to normal statement

--- src/test/java/cz/muni/fi/pv260/videostore/CustomerStatementTest.java
+++ src/test/java/cz/muni/fi/pv260/videostore/CustomerStatementTest.java
@@ -163,6 +163,134 @@ final class CustomerStatementTest {
                 points</p>""");
     }
 
+    void emptyCustomerNameHtml() {
+        customer = new Customer("");
+
+        assertHtmlStatement("""
+                <h1>Rentals for <em></em></h1>
+                <table>
+                <thead>
+                  <tr> <th> Movie <th> Price
+                <tbody>
+                  <tr> <th> You owe <td> 0.0
+                </table>
+                <p>On this rental you earned <strong>0</strong> frequent renter
+                points</p>""");
+    }
+
+    @Test
+    void nullCustomerNameHtml() {
+        customer = new Customer(null);
+
+        assertHtmlStatement("""
+                <h1>Rentals for <em>null</em></h1>
+                <table>
+                <thead>
+                  <tr> <th> Movie <th> Price
+                <tbody>
+                  <tr> <th> You owe <td> 0.0
+                </table>
+                <p>On this rental you earned <strong>0</strong> frequent renter
+                points</p>""");
+    }
+
+    @Test
+    void unicodeCustomerNameHtml() {
+        customer = new Customer("František Vopršálek");
+
+        assertHtmlStatement("""
+                <h1>Rentals for <em>František Vopršálek</em></h1>
+                <table>
+                <thead>
+                  <tr> <th> Movie <th> Price
+                <tbody>
+                  <tr> <th> You owe <td> 0.0
+                </table>
+                <p>On this rental you earned <strong>0</strong> frequent renter
+                points</p>""");
+    }
+
+    @Test
+    void emptyMovieNameHtml() {
+        customer.addRental(new Rental(new RegularMovie("") {
+            @Override
+            public double getPriceOf(int daysRented) {
+                return 5;
+            }
+
+            @Override
+            int getFrequenterPoints(int daysRented) {
+                return 1;
+            }
+        }, 4));
+
+        assertHtmlStatement("""
+                <h1>Rentals for <em>John Doe</em></h1>
+                <table>
+                <thead>
+                  <tr> <th> Movie <th> Price
+                <tbody>
+                  <tr> <td>  <td> 5.0
+                  <tr> <th> You owe <td> 5.0
+                </table>
+                <p>On this rental you earned <strong>1</strong> frequent renter
+                points</p>""");
+    }
+
+    @Test
+    void nullMovieNameHtml() {
+        customer.addRental(new Rental(new RegularMovie(null) {
+            @Override
+            public double getPriceOf(int daysRented) {
+                return 1.5;
+            }
+
+            @Override
+            int getFrequenterPoints(int daysRented) {
+                return 1;
+            }
+        }, 4));
+
+        assertHtmlStatement("""
+                <h1>Rentals for <em>John Doe</em></h1>
+                <table>
+                <thead>
+                  <tr> <th> Movie <th> Price
+                <tbody>
+                  <tr> <td> null <td> 1.5
+                  <tr> <th> You owe <td> 1.5
+                </table>
+                <p>On this rental you earned <strong>1</strong> frequent renter
+                points</p>""");
+    }
+
+    @Test
+    void unicodeMovieNameHtml() {
+        customer.addRental(new Rental(new RegularMovie("Příběhy obyčejného šílenství") {
+            @Override
+            public double getPriceOf(int daysRented) {
+                return 5;
+            }
+
+            @Override
+            int getFrequenterPoints(int daysRented) {
+                return 1;
+            }
+        }, 4));
+
+        assertHtmlStatement("""
+                <h1>Rentals for <em>John Doe</em></h1>
+                <table>
+                <thead>
+                  <tr> <th> Movie <th> Price
+                <tbody>
+                  <tr> <td> Příběhy obyčejného šílenství <td> 5.0
+                  <tr> <th> You owe <td> 5.0
+                </table>
+                <p>On this rental you earned <strong>1</strong> frequent renter
+                points</p>""");
+    }
+
     private void assertStatement(String expectedStatement) {
         assertEquals(expectedStatement, customer.statement());
     }
