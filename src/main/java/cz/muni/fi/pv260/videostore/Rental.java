aa9ccd6c23432db8b848ca5166a5dd00e7bbc0bc
Oskar Adam Valent <xvalent@fi.muni.cz>
2024-04-04 08:40:51 +0200

    Format Rental.java

--- src/main/java/cz/muni/fi/pv260/videostore/Rental.java
+++ src/main/java/cz/muni/fi/pv260/videostore/Rental.java
@@ -1,25 +1,25 @@
 package cz.muni.fi.pv260.videostore;
 
-public class Rental
-{
+public class Rental {
     private final Movie movie;
     private final int daysRented;
 
-    public Rental (Movie movie, int daysRented) {
-        this.movie      = movie;
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
         this.daysRented = daysRented;
     }
 
-    public int getDaysRented () {
+    public int getDaysRented() {
         return daysRented;
     }
 
-    public Movie getMovie () {
+    public Movie getMovie() {
         return movie;
     }
 
     /**
      * Method calculates price for this rental. Price depends on movie type and rental period.
+     *
      * @return rental price.
      */
     public double getRentalPrice() {
@@ -28,6 +28,7 @@ public class Rental
 
     /**
      * Method calculates frequent renter points for this rental.
+     *
      * @return Amount of frequent renter points.
      */
     public int getFrequenterPoints() {
